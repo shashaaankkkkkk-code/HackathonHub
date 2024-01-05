1bafce3af0bb238fda8b7bebd749bec7a8967347
shashaaankkkkk <shashankshekhar8534@gmail.com>
2024-01-05 07:25:22 +0530

    added check

--- HackathonHub/HackathonHub/urls.py
+++ HackathonHub/HackathonHub/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from authentication import views as auth_view
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("",auth_view.hello)
 ]
